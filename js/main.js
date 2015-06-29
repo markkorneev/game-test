a33ca1bf82efe4ccebe453150108967334971df1
Mark Korneev <korneevmark@gmail.com>
2015-06-29 03:38:53 +0300

    add licence info to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+/**
+ * @author Mark Korneev <korneevmark@gmail.com>
+ * @license GNU/AGPLv3
+ * @see {@link https://www.gnu.org/licenses/agpl-3.0.txt|License}
+ */
+
 'use strict';
 
 (function () {
